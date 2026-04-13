9d7bf7121557ed40cd03604be2096ef0b44df6c9
israclaude-maker <israclaude@gmail.com>
2026-04-13 12:39:07 +0500

    apni changes ka description yahan likho

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -140,6 +140,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
     "https://*.railway.app",
     "https://web-production-cef22.up.railway.app",
+    "https://sky-chat.duckdns.org",
 ]
 # File Upload Settings
 DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50MB
